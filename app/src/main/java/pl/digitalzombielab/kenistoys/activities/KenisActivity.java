70af30d645ac85897d779f6a57f7409b993efd7c
migellal <michalgellert@gmail.com>
2016-01-31 20:54:47 +0100

    Part 12 final

--- app/src/main/java/pl/digitalzombielab/kenistoys/activities/KenisActivity.java
+++ app/src/main/java/pl/digitalzombielab/kenistoys/activities/KenisActivity.java
@@ -1,5 +1,6 @@
 package pl.digitalzombielab.kenistoys.activities;
 
+import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,11 +8,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import pl.digitalzombielab.kenistoys.CommonColors;
 import pl.digitalzombielab.kenistoys.R;
 
 public class KenisActivity extends AppCompatActivity implements CommonColors {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +26,20 @@ public class KenisActivity extends AppCompatActivity implements CommonColors {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setNaviBarColor();
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        final Activity activity = this;
+        webView.setWebViewClient(new WebViewClient() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                Toast.makeText(activity, description, Toast.LENGTH_LONG).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        webView.loadUrl("http://www.kenis.pl");
+        setContentView(webView);
     }
 
     @Override
